8c14d9f8703e61cf008b1a953a65767e288e9461
JinQing Lee <hourout@163.com>
2018-11-15 16:03:30 +0800

    add nem feature：custom metrics name

--- beefly/keras_callbacks_plot.py
+++ beefly/keras_callbacks_plot.py
@@ -22,9 +22,11 @@ def draw(metrics, logs, epoch, columns, iter_num, mode, wait_num, xlabel, figsiz
         plt.show()
 
 class PlotMetrics(tf.keras.callbacks.Callback):
-    def __init__(self, columns=2, iter_num=None, mode=1, wait_num=1, figsize=None, cell_size=(6, 4), valid_fmt="val_{}"):
+    def __init__(self, metrics_name, columns=2, iter_num=None, mode=1, wait_num=1, figsize=None, cell_size=(6, 4),
+                 valid_fmt="val_{}"):
         assert (mode==1)|(mode==0), 'please mode input 0 or 1'
         tf.logging.set_verbosity(tf.logging.ERROR)
+        self.metrics_name = metrics_name
         self.columns = columns
         self.iter_num = iter_num
         self.mode = mode
@@ -36,41 +38,52 @@ class PlotMetrics(tf.keras.callbacks.Callback):
         self.batch_logs = defaultdict(list)
         self.xlabel = {0:'epoch', 1:'batch'}
         self.batch_num = 1
-
+        
     def on_epoch_begin(self, epoch, logs=None):
         pass
 
     def on_epoch_end(self, epoch, logs=None):
         if self.mode==0:
+            old_all_name = self.model.metrics_names+['val_'+i for i in self.model.metrics_names]
+            new_all_name = self.metrics_name+[self.valid_fmt.split('_')[0]+'_'+i for i in self.metrics_name]
+            for old_name, new_name in zip(old_all_name, new_all_name):
+                logs[new_name] = logs.pop(old_name)
             self.metrics = list(filter(lambda x: self.valid_fmt.split('_')[0] not in x.lower(), logs))
             if self.figsize is None:
                 self.figsize = (self.columns*self.cell_size[0], ((len(self.metrics)+1)//self.columns+1)*self.cell_size[1])
             for metric in logs:
                 self.epoch_logs[metric] += [logs[metric]]
-            draw(metrics=self.metrics, logs=self.epoch_logs, epoch=epoch, columns=self.columns, iter_num=self.iter_num,
-                 mode=self.mode, wait_num=self.wait_num, xlabel=self.xlabel, figsize=self.figsize, cell_size=self.cell_size, valid_fmt=self.valid_fmt)
+            draw(metrics=self.metrics, logs=self.epoch_logs, epoch=epoch, columns=self.columns,
+                 iter_num=self.iter_num, mode=self.mode, wait_num=self.wait_num, xlabel=self.xlabel,
+                 figsize=self.figsize, cell_size=self.cell_size, valid_fmt=self.valid_fmt)
     
     def on_batch_begin(self, batch, logs=None):
         pass
 
     def on_batch_end(self, batch, logs=None):
         if self.mode==1:
+            loss = self.model.test_on_batch(self.validation_data[0], self.validation_data[1])
+            [self.valid_fmt.split('_')[0]+'_'+i for i in self.metrics_names]
+#             logs['']
             self.metrics = list(filter(lambda x: x not in ['batch', 'size'], logs))
             if self.figsize is None:
                 self.figsize = (self.columns*self.cell_size[0], ((len(self.metrics)+1)//self.columns+1)*self.cell_size[1])
             for metric in self.metrics:
                 self.batch_logs[metric] += [logs[metric]]
             self.batch_num += batch
-            draw(metrics=self.metrics, logs=self.batch_logs, epoch=self.batch_num, columns=self.columns, iter_num=self.iter_num,
-                 mode=self.mode, wait_num=self.wait_num, xlabel=self.xlabel, figsize=self.figsize, cell_size=self.cell_size, valid_fmt=self.valid_fmt)
+            draw(metrics=self.metrics, logs=self.batch_logs, epoch=self.batch_num, columns=self.columns,
+                 iter_num=self.iter_num, mode=self.mode, wait_num=self.wait_num, xlabel=self.xlabel,
+                 figsize=self.figsize, cell_size=self.cell_size, valid_fmt=self.valid_fmt)
 
     def on_train_begin(self, logs=None):
         pass
 
     def on_train_end(self, logs=None):
         if self.mode==0:
-            draw(metrics=self.metrics, logs=self.epoch_logs, epoch=self.wait_num, columns=self.columns, iter_num=self.iter_num,
-                 mode=self.mode, wait_num=self.wait_num, xlabel=self.xlabel, figsize=self.figsize, cell_size=self.cell_size, valid_fmt=self.valid_fmt)
+            draw(metrics=self.metrics, logs=self.epoch_logs, epoch=self.wait_num, columns=self.columns,
+                 iter_num=self.iter_num, mode=self.mode, wait_num=self.wait_num, xlabel=self.xlabel,
+                 figsize=self.figsize, cell_size=self.cell_size, valid_fmt=self.valid_fmt)
         else:
-            draw(metrics=self.metrics, logs=self.batch_logs, epoch=self.wait_num, columns=self.columns, iter_num=self.iter_num,
-                 mode=self.mode, wait_num=self.wait_num, xlabel=self.xlabel, figsize=self.figsize, cell_size=self.cell_size, valid_fmt=self.valid_fmt)
+            draw(metrics=self.metrics, logs=self.batch_logs, epoch=self.wait_num, columns=self.columns,
+                 iter_num=self.iter_num, mode=self.mode, wait_num=self.wait_num, xlabel=self.xlabel,
+                 figsize=self.figsize, cell_size=self.cell_size, valid_fmt=self.valid_fmt)
