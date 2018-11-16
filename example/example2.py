58147f648b8e6f96e9c837ed0e5bd5bf79bf0ad8
JinQing Lee <hourout@163.com>
2018-11-16 11:18:32 +0800

    Update example2.py

--- example/example2.py
+++ example/example2.py
@@ -1,72 +1,44 @@
-import numpy as np
 import beefly
+import numpy as np
 import tensorflow as tf
 
 
-def generator(latent_dim=100, image_shape=(28,28,1)):
-    noise = tf.keras.Input(shape=(latent_dim,))
-    x = tf.keras.layers.Dense(256)(noise)
-    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
-    x = tf.keras.layers.BatchNormalization(momentum=0.8)(x)
-    x = tf.keras.layers.Dense(512)(x)
-    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
-    x = tf.keras.layers.BatchNormalization(momentum=0.8)(x)
-    x = tf.keras.layers.Dense(1024)(x)
-    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
-    x = tf.keras.layers.BatchNormalization(momentum=0.8)(x)
-    x = tf.keras.layers.Dense(np.prod(image_shape), activation='tanh')(x)
-    image = tf.keras.layers.Reshape(image_shape)(x)
-    gnet = tf.keras.Model(noise, image)
-    return gnet
-
-def discriminator(image_shape=(28,28,1)):
+def simple_mnist(batch_num=1000, batch_size=32, image_shape=(28,28,1)):
     image = tf.keras.Input(shape=image_shape)
-    x = tf.keras.layers.Flatten()(image)
-    x = tf.keras.layers.Dense(512)(x)
-    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
-    x = tf.keras.layers.Dense(256)(x)
-    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
-    logit = tf.keras.layers.Dense(1, activation='sigmoid')(x)
-    dnet = tf.keras.Model(image, logit)
-    return dnet
-
-def train(batch_num=10000, batch_size=128, latent_dim=100, image_shape=(28,28,1)):
-    tf.logging.set_verbosity(tf.logging.ERROR)
-    dnet = discriminator(image_shape)
-    dnet.compile(loss='binary_crossentropy', 
-                 optimizer=tf.keras.optimizers.Adam(0.0002, 0.5),
-                 metrics=['accuracy'])
-
-    noise = tf.keras.Input(shape=(latent_dim,))
-    gnet = generator(latent_dim, image_shape)
-    dnet.trainable = False
-    image = gnet(noise)
-    logit = dnet(image)
-    gan = tf.keras.Model(noise, logit)
-    gan.compile(loss='binary_crossentropy',
-                optimizer=tf.keras.optimizers.Adam(0.0002, 0.5),
-                metrics=['accuracy'])
-
-    (X_train, _), (_, _) = tf.keras.datasets.fashion_mnist.load_data()
-    X_train = X_train / 127.5 - 1.
-    X_train = np.expand_dims(X_train, axis=3)
+    x = tf.keras.layers.Conv2D(64, 3)(image)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.ReLU()(x)
+    x = tf.keras.layers.MaxPool2D(2)(x)
+    x = tf.keras.layers.Conv2D(64, 3)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.ReLU()(x)
+    x = tf.keras.layers.Flatten()(x)
+    logit = tf.keras.layers.Dense(10, activation='softmax')(x)
+    model = tf.keras.Model(image, logit)
+    model.compile(loss=['sparse_categorical_crossentropy'], 
+                  optimizer=tf.keras.optimizers.Adam(0.001),
+                  metrics=['accuracy'])
     
-    beeplot = beefly.plot_metrics(valid_fmt="G_{}")
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.fashion_mnist.load_data()
+    X_train = np.expand_dims(X_train/127.5-1., axis=3)
+    X_test = np.expand_dims(X_test/127.5-1., axis=3)
+    y_train = y_train.reshape(-1, 1)
+    y_test = y_test.reshape(-1, 1)
+    
+    beeplot = beefly.plot_metrics()
     for batch in range(batch_num):
-        batch_image = X_train[np.random.choice(range(X_train.shape[0]), batch_size, False)]
-        batch_noise = np.random.normal(0, 1, (batch_size, latent_dim))
-        batch_gen_image = gnet.predict(batch_noise)
-
-        d_loss_real = dnet.train_on_batch(batch_image, np.ones((batch_size, 1)))
-        d_loss_fake = dnet.train_on_batch(batch_gen_image, np.zeros((batch_size, 1)))
-        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-        g_loss = gan.train_on_batch(batch_noise, np.ones((batch_size, 1)))
-        if batch%1==0:
-            beeplot.update({'loss': d_loss[0], 'binary_acc': d_loss[1],
-                            'G_loss': g_loss[0], 'G_binary_acc': g_loss[1]})
+        train_random = np.random.choice(range(X_train.shape[0]), batch_size, False)
+        test_random = np.random.choice(range(X_test.shape[0]), batch_size, False)
+        train_batch_image, train_batch_label = X_train[train_random], y_train[train_random]
+        test_batch_image, test_batch_label = X_test[test_random], y_test[test_random]
+        loss = model.train_on_batch(train_batch_image, train_batch_label)
+        result = model.test_on_batch(test_batch_image, test_batch_label)
+        if batch%2==0:
+            beeplot.update({'train_loss':loss[0], 'train_accuracy': loss[1],
+                            'test_loss': result[0], 'test_accuracy': result[1]})
             beeplot.draw()
-    return gnet
+    return model
 
 
 if __name__ == '__main__':
-    gnet = train()
+    model = simple_mnist()
