db996923d322eb114114f27dd76dfca0e4860077
JinQing Lee <hourout@163.com>
2018-11-17 08:51:50 +0800

    add doc

--- beefly/general_plot.py
+++ beefly/general_plot.py
@@ -1,14 +1,27 @@
-import warnings
-
-import matplotlib
 import matplotlib.pyplot as plt
 from collections import defaultdict
 from IPython.display import clear_output
 
 
-
 class plot_metrics():
     def __init__(self, columns=2, iter_num=None, mode=1, wait_num=1, figsize=None, cell_size=(6, 4), valid_fmt="val_{}"):
+        """
+        Arguments:
+            columns：int，default 2，The number of sub graphs that the width of metrics
+                     visualiztion image to accommodate at most；
+            iter_num：int，default None，Pre-specify the maximum value of x-axis in each
+                      sub-picture to indicate the maximum number of batch or epoch training;
+            mode：int，default 1，1 means the x-axis name is 'batch', 0 means the x-axis name is 'epoch';
+            wait_num：int，default 1，Indicates how many batches or epochs are drawn
+                      each time a graph is drawn;
+            figsize：tuple，default None，Represents the customize image size;
+            cell_size：tuple，default (6, 4)，Indicates the customize image size,
+                       which is used when figsize=None;
+            valid_fmt：str，default "val_{}",The string preceding the underscore is used to
+                       instruction the training and validation is displayed together in the
+                       same sub graph. The training indicator is not required to have a prefix.
+                       The validation indicator prefix is 'val' in the "val_{}";
+        """
         self.columns = columns
         self.iter_num = iter_num
         self.mode = mode
